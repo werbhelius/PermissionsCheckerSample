2051a21154e1a622fa899589c38babaacb38ec48
wanbo <wanbowb@live.com>
2017-01-13 14:32:36 +0800

    update intent

--- app/src/main/java/com/werb/permissionscheckersample/MainActivity.java
+++ app/src/main/java/com/werb/permissionscheckersample/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
@@ -16,6 +18,9 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.werb.permissionschecker.PermissionChecker;
+
+import java.io.File;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void open() {
-        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivity(intent);
     }
 
